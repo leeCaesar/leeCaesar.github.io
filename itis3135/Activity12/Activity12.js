c356f20dcf7cb0bd86889e73d74bd6fd414cbb0d
[leeCaesar] <[lcaesar@uncc.edu]>
2021-11-13 14:53:51 -0500

    11/13

---
+++ itis3135/Activity12/Activity12.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $("#slider").bxSlider({
+        auto: true,
+        pause: 3000,
+        randomStart: true,
+        captions: true,
+        pager: true,
+        minSlides: 1,
+        maxSlides: 1,
+        slideWidth: 500,
+        slideMargin: 20
+    });
+});
