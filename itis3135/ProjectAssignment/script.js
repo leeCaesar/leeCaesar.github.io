5137e971b16ee858b08bf7f44046a6634ba63a42
[leeCaesar] <[lcaesar@uncc.edu]>
2021-12-12 18:45:11 -0500

    12/12

--- itis3135/ProjectAssignment/script.js
+++ itis3135/ProjectAssignment/script.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
-    const url = "";
+    const url = "https://leecaesar.github.io/itis3135/ProjectAssignment/aboutme.json";
+
+    $.getJSON(url, function(data) {
+        $.each(data, function()  {
+            $.each(this, function(key, value) {
+                $("#aboutme").append(
+                    '<img src="' + value.image + '"><br />' + 
+                        '<h2>' + value.Name + '</h2><br />' + 
+                        '<h3>' + value.title + '</h3><br />' + 
+                        '<p>' + value.about + '</p><br />' +
+                        '<div class="aboutme-links">' + 
+                        '<a href="' + value.IG + '">Instagram  </a>' +
+                        '<a href="' + value.SC + '">Soundcloud</a>' +
+                        '</div>'
+                );
+            });
+        });
+    });
 });
