1fead946eedd36326aaa58d575057e40abcfdf87
[leeCaesar] <[lcaesar@uncc.edu]>
2021-12-12 19:01:04 -0500

    Update script.js

--- itis3135/ProjectAssignment/script.js
+++ itis3135/ProjectAssignment/script.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
         $.each(data, function()  {
             $.each(this, function(key, value) {
                 $("#aboutme").append(
-                    '<img src="' + value.image + '"><br />' + 
+                    '<img src="' + value.image + '" alt="placeholder for photo"><br />' + 
                         '<h2>' + value.Name + '</h2><br />' + 
                         '<h3>' + value.title + '</h3><br />' + 
                         '<p>' + value.about + '</p><br />' +
@@ -13,20 +13,14 @@ $(document).ready(function() {
                         '<a href="' + value.IG + '">Instagram  </a>' +
                         '<a href="' + value.SC + '">Soundcloud</a>' +
                         '</div>'
-                );
-            });
-        });
-    });
-    $.getJSON(Url, function(data) {
-        $.each(data, function()  {
-            $.each(this, function(key, value) {
+                    );
                 $("#nav").append(
-                        '<div class="aboutme-links">' + 
+                   '<div class="aboutme-links">' + 
                         '<a href="' + value.IG + '">Instagram  </a>' +
                         '<a href="' + value.SC + '">Soundcloud</a>' +
                         '</div>'
                     );
-                });
             });
         });
+    });
 });
