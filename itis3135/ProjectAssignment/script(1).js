c1c0c6601e73129af77318eec352556aa6ac4390
[leeCaesar] <[lcaesar@uncc.edu]>
2021-12-12 19:15:01 -0500

    12/12

--- itis3135/ProjectAssignment/script(1).js
+++ itis3135/ProjectAssignment/script(1).js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 const Url = "https://leecaesar.github.io/itis3135/ProjectAssignment/index.json";
-
     $.getJSON(Url, function(data) {
         $.each(data, function()  {
             $.each(this, function(key, value) {
                 $("#index").append(
+                    '<img src="' + value.image + '" alt="placeholder for photo"/><br />' +
                     '<div class="aboutme-links">' + 
                          '<a href="' + value.IG + '">Instagram  </a>' +
                          '<a href="' + value.SC + '">Soundcloud</a>' +
